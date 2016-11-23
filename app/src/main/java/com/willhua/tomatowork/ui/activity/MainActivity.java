9cc526e33a295257ec804ff159e36bee7dd19565
willhua <znjxboy@163.com>
2016-11-23 23:29:35 +0800

    tab view event

--- app/src/main/java/com/willhua/tomatowork/ui/activity/MainActivity.java
+++ app/src/main/java/com/willhua/tomatowork/ui/activity/MainActivity.java
@@ -3,6 +3,7 @@ package com.willhua.tomatowork.ui.activity;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
 
 import com.willhua.tomatowork.R;
 import com.willhua.tomatowork.presenter.MainPresenter;
@@ -13,29 +14,45 @@ import com.willhua.tomatowork.ui.fragment.TabFragment;
  * Created by willhua on 2016/11/13.
  */
 
-public class MainActivity extends BaseActivity implements TabFragment.TabSelected{
+public class MainActivity extends BaseActivity implements TabFragment.TabSelected {
 
     private Toolbar mToolbar;
     private MainPresenter mMainPresenter;
     private ViewPager mViewPager;
+    private TabFragment mTabFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
 
-        initToolbar();
+        initView();
         mViewPager = (ViewPager) findViewById(R.id.viewpager);
         mViewPager.setAdapter(new FunctionPagerAdapter(this));
     }
 
-    private void initToolbar(){
+    private void initView(){
         mToolbar = (Toolbar)findViewById(R.id.toolbar);
         mToolbar.inflateMenu(R.menu.menu_main);
     }
 
     @Override
-    public void onTabSelected(int index) {
+    public void onStartTomato() {
+        Toast.makeText(getBaseContext(), "start", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onShowCandy() {
+
+    }
+
+    @Override
+    public void onShowStatistics() {
+
+    }
+
+    @Override
+    public void onShowUser() {
 
     }
 }
--- app/src/main/java/com/willhua/tomatowork/ui/fragment/TabFragment.java
+++ app/src/main/java/com/willhua/tomatowork/ui/fragment/TabFragment.java
@@ -1,5 +1,6 @@
 package com.willhua.tomatowork.ui.fragment;
 
+import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -15,17 +16,67 @@ import com.willhua.tomatowork.R;
 
 public class TabFragment extends Fragment {
     public interface TabSelected{
-        void onTabSelected(int index);
+        void onStartTomato();
+        void onShowCandy();
+        void onShowStatistics();
+        void onShowUser();
+    }
+
+    private TabSelected mTabSelected;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if(context instanceof  TabSelected){
+            mTabSelected = (TabSelected)context;
+        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         super.onCreateView(inflater, container, savedInstanceState);
         View view = inflater.inflate(R.layout.tab_layout, null);
-
-
-
+        setClick(view);
         return  view;
+    }
+
+    private void setClick(View root){
+        View view = root.findViewById(R.id.list);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mTabSelected != null){
+                    mTabSelected.onShowCandy();
+                }
+            }
+        });
+        view = root.findViewById(R.id.statistics);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mTabSelected != null){
+                    mTabSelected.onShowStatistics();
+                }
+            }
+        });
+        view = root.findViewById(R.id.start);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mTabSelected != null){
+                    mTabSelected.onStartTomato();
+                }
+            }
+        });
+        view = root.findViewById(R.id.user);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mTabSelected != null){
+                    mTabSelected.onShowUser();
+                }
+            }
+        });
     }
 
 
