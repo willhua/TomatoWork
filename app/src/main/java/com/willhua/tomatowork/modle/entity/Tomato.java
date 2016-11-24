b1b3f8653baa242aa842d2477fb1514d8c984060
willhua <znjxboy@163.com>
2016-11-24 20:25:29 +0800

    done tomato

--- app/src/main/java/com/willhua/tomatowork/modle/entity/Tomato.java
+++ app/src/main/java/com/willhua/tomatowork/modle/entity/Tomato.java
@@ -1,10 +1,72 @@
 package com.willhua.tomatowork.modle.entity;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  * Created by willhua on 2016/11/13.
  */
 
 public class Tomato {
-    private int mTimeLong;
+    public interface TomatoEvent{
+        void onSecond(int left);
+        void onOver();
+    }
 
+    private static final int SECOND = 1000;
+    private static Tomato sSingleton = new Tomato();
+    private TomatoEvent mTomatoEvent;
+    private int mTimeLong;
+    private Timer mTimer;
+    private int mLeftTime;
+    private TimerTask mTimerTask = new TimerTask() {
+        @Override
+        public void run() {
+            if(mTomatoEvent != null){
+                if(mLeftTime == 0){
+                    mTomatoEvent.onOver();
+                }else{
+                    mTomatoEvent.onSecond(--mLeftTime);
+                }
+            }
+        }
+    };
+
+    private Tomato(){
+        mTimer = new Timer();
+    }
+
+    public static Tomato getInstance(){
+        return sSingleton;
+    }
+
+    /**
+     * set time on how long a tomao will last
+     * @param minutes the unit is minute
+     */
+    public void setMinutes(int minutes){
+        if(minutes < 1){
+            minutes = 25;
+        }
+        minutes *= 60;
+        mTimeLong = minutes;
+    }
+
+    /**
+     * set the listener for tomato.
+     * @param tomatoEvent
+     */
+    public void setTomatoEvent(TomatoEvent tomatoEvent) {
+        mTomatoEvent = tomatoEvent;
+    }
+
+    public void startTomato(){
+        mLeftTime = mTimeLong;
+        mTimer.purge();
+        mTimer.schedule(mTimerTask, 0, SECOND);
+    }
+
+    public void cancleTomato(){
+        mTimer.purge();
+    }
 }
--- app/src/main/java/com/willhua/tomatowork/ui/fragment/WorkFragment.java
+++ app/src/main/java/com/willhua/tomatowork/ui/fragment/WorkFragment.java
@@ -1,10 +1,42 @@
 package com.willhua.tomatowork.ui.fragment;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by willhua on 2016/11/20.
  */
 
 public class WorkFragment extends Fragment {
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
 }
